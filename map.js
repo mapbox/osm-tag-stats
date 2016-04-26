5f1736fca6130f05fcb017a714b2c828b155b040
arunasank <aruna@mapbox.com>
2016-04-26 14:35:00 +0530

    Dates

--- map.js
+++ map.js
@@ -1,20 +1,37 @@
 'use strict';
 var turf = require('turf');
 
-module.exports = function(data, tile, writeData, done) {
+module.exports = function (data, tile, writeData, done) {
     var layer = data.osm.osm;
     var osmID = [];
-    var users = ["ruthmaben","jinalfoflia","saikabhi","Jothirnadh","aarthykc","pratikyadav","Chetan_Gowda","oini","ramyaragupathy","nikhilprabhakar","srividya_c","PlaneMad","manings","nammala","poornibadrinath","geohacker","shvrm","bkowshik","sanjayb","Arunasank"];
+    var dates = parseDates(mapOptions.dates);
+    var users = mapOptions.users;
     var result = layer.features.filter(function(val) {
-        if (val.properties['ref']  && val.properties['highway'] === 'motorway_junction' && val.properties['_timestamp'] && users.indexOf(val.properties['_user']) > -1 && (val.properties['_timestamp'] >= 1459362600 && val.properties['_timestamp'] <= 1460658600)) {
-            osmID.push(val.properties['_osm_way_id'] ? val.properties['_osm_way_id'] : val.properties['_osm_node_id']);
+        if (val.properties['ref']  && val.properties['highway'] === 'motorway_junction' && val.properties['_timestamp'] && users.indexOf(val.properties['_user']) > -1 && (val.properties['_timestamp'] >= dates[0] && val.properties['_timestamp'] <= dates[1])) {
+
             return true;
         }
     });
 
-    if (result.length > 0) {
+    if (result.length > 0 && mapOptions.geojson) {
         var fc = turf.featurecollection(result);
         writeData(JSON.stringify(fc.features[0]) + '\n');
     }
     done(null, osmID);
 };
+
+function parseDates(dates) {
+    var startDate = new Date(dates[0]);
+    // console.log(startDate);
+    // console.log(new Date(startDate.getTime()));
+    var endDate = new Date(dates[dates.length - 1]);
+    if (startDate === endDate) {
+        startDate.setHours(5);
+        startDate.setMinutes(30);
+        endDate.setDate(endDate.getDate() + 1);
+        endDate.setHours(5);
+        endDate.setMinutes(30);
+    }
+
+    return [startDate.getTime(), endDate.getTime()];
+}
