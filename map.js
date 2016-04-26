627172cf0328fe01b15d27444f55cc54e0ac26ff
arunasank <aruna@mapbox.com>
2016-04-26 13:15:02 +0530

    Appease the linter

--- map.js
+++ map.js
@@ -6,16 +6,15 @@ module.exports = function(data, tile, writeData, done) {
     var osmID = [];
     var users = ["ruthmaben","jinalfoflia","saikabhi","Jothirnadh","aarthykc","pratikyadav","Chetan_Gowda","oini","ramyaragupathy","nikhilprabhakar","srividya_c","PlaneMad","manings","nammala","poornibadrinath","geohacker","shvrm","bkowshik","sanjayb","Arunasank"];
     var result = layer.features.filter(function(val) {
-        if(val.properties['ref']  && val.properties['highway'] === 'motorway_junction' && val.properties['_timestamp'] && users.indexOf(val.properties['_user']) > -1
-           && (val.properties['_timestamp'] >= 1459362600 && val.properties['_timestamp'] <= 1460658600)){
+        if (val.properties['ref']  && val.properties['highway'] === 'motorway_junction' && val.properties['_timestamp'] && users.indexOf(val.properties['_user']) > -1 && (val.properties['_timestamp'] >= 1459362600 && val.properties['_timestamp'] <= 1460658600)) {
             osmID.push(val.properties['_osm_way_id'] ? val.properties['_osm_way_id'] : val.properties['_osm_node_id']);
             return true;
-	}
+        }
     });
 
     if (result.length > 0) {
-	var fc = turf.featurecollection(result);
-	writeData(JSON.stringify(fc.features[0]) + '\n');
+        var fc = turf.featurecollection(result);
+        writeData(JSON.stringify(fc.features[0]) + '\n');
     }
     done(null, osmID);
 };
