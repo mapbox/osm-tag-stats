a67e02c43c1b8b545d1c12903723063070b0ef35
arunasank <aruna@mapbox.com>
2016-04-26 13:13:23 +0530

    Fix unnecessary booleans, lint

--- index.js
+++ index.js
@@ -4,28 +4,27 @@ var tileReduce = require('tile-reduce');
 var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
 var _ = require('underscore');
-var count = (argv.count) ? true : false;
-var geojson = (argv.geojson) ? true : false;
-var users = (argv.users) ? argv.users.split(",") : ["ruthmaben","jinalfoflia","saikabhi","Jothirnadh","aarthykc","pratikyadav","Chetan_Gowda","oini","ramyaragupathy","nikhilprabhakar","srividya_c","PlaneMad","manings","nammala","poornibadrinath","geohacker","shvrm","bkowshik","sanjayb","Arunasank"];
-var dates = (argv.date) ? argv.date.split(",") : false;
-
-var OSMID= [];
+var count = Boolean(argv.count);
+var geojson = Boolean(argv.geojson);
+var users = (argv.users) ? argv.users.split(', ') : ['ruthmaben', 'jinalfoflia', 'saikabhi', 'Jothirnadh', 'aarthykc', 'pratikyadav', 'Chetan_Gowda', 'oini', 'ramyaragupathy', 'nikhilprabhakar', 'srividya_c', 'PlaneMad', 'manings', 'nammala', 'poornibadrinath', 'geohacker', 'shvrm', 'bkowshik', 'sanjayb', 'Arunasank'];
+var dates = (argv.date) ? argv.date.split(', ') : false;
+var OSMID = [];
 
 tileReduce({
-  zoom: 12,
-  map: path.join(__dirname, 'map.js'),
-  sources: [{name: 'osm', mbtiles: path.join(__dirname, '../latest.planet.mbtiles')}],
-  mapOptions: { 
-    "count":count,
-    "geojson":geojson,
-    "dates":dates,
-    "users":users
-  }
+    zoom: 12,
+    map: path.join(__dirname, 'map.js'),
+    sources: [{name: 'osm', mbtiles: path.join(__dirname, '../latest.planet.mbtiles')}],
+    mapOptions: {
+        'count': count,
+        'geojson': geojson,
+        'dates': dates,
+        'users': users
+    }
 })
-.on('reduce', function(id) {
-OSMID = OSMID.concat(id);
+.on('reduce', function (id) {
+    OSMID = OSMID.concat(id);
 })
-.on('end', function() {
-  var uniqueIDs = _.uniq(OSMID);
-  console.log('Features total: %d', uniqueIDs.length);
+.on('end', function () {
+    var uniqueIDs = _.uniq(OSMID);
+    console.log('Features total: %d', uniqueIDs.length);
 });
